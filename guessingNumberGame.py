504a1b5b3a269b4114f36cf9c326326df8d7deff
Francesco Mercuri <fc.mercuri@gmail.com>
2025-09-30 14:49:58 +0100

    Update guessingNumberGame.py

--- guessingNumberGame.py
+++ guessingNumberGame.py
@@ -1,15 +1,17 @@
+import random
+
 print("welcome to number guessing game")
 
-mynum = 456
+mynum = random.randint(1, 10)
 counter = 0
 
 while True:
-  num = int(input("guess a num between 1 to 1000: "))
-  if num > 456:
+  num = int(input("guess a num between 1 to 10: "))
+  if num > mynum:
     print("num is too high. try again")
     counter += 1
     continue
-  elif num < 456 and num > 0:
+  elif num < mynum and num > 0:
     print("num is too low. try again")
     counter += 1
     continue
