bd21aee3d9fe93b02e2a34c53c0d4c006f5a67b9
Francesco Mercuri <fc.mercuri@gmail.com>
2025-10-01 10:01:20 +0100

    Add files via upload

---
+++ ForRange.py
@@ -0,0 +1,15 @@
+for i in range (100,110):
+ print(i)
+
+for i in range (100,110, 2):
+ print(i)
+
+#reverse order
+for i in range (10, 0, -1):
+ print(i)
+
+for i in range (7):
+ print("Day", i)
+
+for i in range (1,12):
+ print(i, "x 13 =", i*13)
