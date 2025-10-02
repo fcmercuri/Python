98a3328ca9b77ed14ba13b1889ea3ead3400c358
Francesco Mercuri <fc.mercuri@gmail.com>
2025-10-02 14:09:28 +0100

    Create SubroutineParameters.py

---
+++ SubroutineParameters.py
@@ -0,0 +1,9 @@
+def whichCake(ingredient):
+ if ingredient == "chocolate":
+  print("This is the recipe for a chocolate cake....")
+ elif ingredient == "broccoli":
+  print("This is the recipe for a broccoli cake....")
+ else:
+  print("I don't know that ingredient")
+
+whichCake("broccoli")
