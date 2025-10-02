19042d6d1bbd9c05c2754532db5c14f3c5c2e3fc
Francesco Mercuri <fc.mercuri@gmail.com>
2025-10-02 14:16:30 +0100

    Update SubroutineParameters.py

--- SubroutineParameters.py
+++ SubroutineParameters.py
@@ -7,4 +7,6 @@ def whichCake(ingredient, base):
   print("I don't know that ingredient")
  print("so you want", ingredient, "cake on a", base)
 
-whichCake("funghi", "bread")
+userIngredient = input("ingredient?")
+userBase = input("base?")
+whichCake(userIngredient, userBase)
