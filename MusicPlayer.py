ae9e1d9643dfec59f8549dd5165f519d9e41d8d5
Francesco Mercuri <fc.mercuri@gmail.com>
2025-10-03 16:28:26 +0100

    Create MusicPlayer.py

---
+++ MusicPlayer.py
@@ -0,0 +1,26 @@
+import os, time
+
+print("MyPOD Music Player")
+
+def player():
+    print("Press 1 to Play")
+    print("Press 2 to Exit")
+    print("Press other number to see the menu again")
+    
+    press = int(input("Choose an option: "))
+
+    return press
+
+while True:
+    press = player()
+    if press == 1:
+        print("Playing Music 🎵")
+        print()
+        time.sleep(1)
+        os.system("clear")
+    elif press == 2:
+        print("Exit")
+        print()
+        break
+    else:
+        print("Invalid option. Here's the menu again: ")
