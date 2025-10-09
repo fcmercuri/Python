5831ffddfeeb7eca4edc8789f76605289607b865
Francesco Mercuri <fc.mercuri@gmail.com>
2025-10-09 12:41:22 +0100

    Update DynamicList.py

--- DynamicList.py
+++ DynamicList.py
@@ -1,1 +1,15 @@
-my
+myAgenda =[]
+
+def printList():
+  for item in myAgenda:
+    print(item)
+
+while True:
+  menu = input("Add or remove?: ")
+  if menu == "add":
+    item = input("Item to add: ")
+    myAgenda.append(item)
+  elif menu == "remove":
+    item = input("Item to remove: ")
+    myAgenda.remove(item)
+  printList()
