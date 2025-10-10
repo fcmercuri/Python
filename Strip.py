3a5da93214c9d52d34d9a1a448f586ffc3dca239
Francesco Mercuri <fc.mercuri@gmail.com>
2025-10-10 12:24:43 +0100

    Create Strip.py

---
+++ Strip.py
@@ -0,0 +1,6 @@
+name = input("What's your name?: ")
+if name.lower().strip() == "alice": #strip remove space when user input
+#upper/ title/ capitalise 
+       print("Hello, Alice!")
+else:
+       print("Hello, stranger!")
